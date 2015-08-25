43a4a16fdde17f119819e335a3ccdedaf61392c4
Dexter Tan <dexter.c.tan@jpl.nasa.gov>
2015-08-25 18:03:37 +0000

    added shell script

---
+++ update_figure-34-16.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+BASE_PATH=$(dirname "${BASH_SOURCE}")
+BASE_PATH=$(cd "${BASE_PATH}"; pwd)
+
+source $BASE_PATH/env/bin/activate
+python $BASE_PATH/syncer_scripts/update_figure-34-16.py $*
